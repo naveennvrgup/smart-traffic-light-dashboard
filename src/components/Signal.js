621878ed6afdea8105982a80cc474bd51cb0f34a
Naveen Sundar <naveennvrgup@gmail.com>
2020-10-19 23:22:51 +0530

    feat: timer api working flawless

--- src/components/Signal.js
+++ src/components/Signal.js
@@ -1,33 +1,95 @@
-import React, {Fragment} from 'react';
-import axios from '../axios';
+import React, {useState, useEffect, Fragment} from 'react';
+import './signal.css';
 
-const Signal = ({signal}) => {
-    let {lights, timer} = signal;
-    const time = new Date();
-    time.setHours(parseInt(timer.slice(0, 2)));
-    time.setMinutes(parseInt(timer.slice(3, 5)));
-    time.setSeconds(parseInt(timer.slice(6, 8)));
 
+const Signal = ({signal, resetInterval, currTime}) => {
+    const {lights} = signal;
+    const syncTime = new Date(signal.timer)
+
+    const [ratioTimeModulo, setRatioTimeModulo] = useState(0)
+
+    useEffect(() => {
+        const intervalObj = setInterval(() => {
+            const diff = Math.ceil(currTime.valueOf() / 1000 - syncTime.valueOf() / 1000)
+            setRatioTimeModulo(diff % resetInterval)
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalObj);
+        }
+    })
+
+
+    const timeBarWidth = (ratioTimeModulo / resetInterval) * 90;
+    let timeBarColor;
+    if (timeBarWidth < 40) timeBarColor = 'lightgreen';
+    else if (timeBarWidth < 70) timeBarColor = 'yellow';
+    else timeBarColor = 'orange';
+
+
+    const timerBarStyle = {
+        width: timeBarWidth + '%',
+        background: timeBarColor,
+    }
 
     const main = <Fragment>
-        <td rowSpan={lights.length}>{signal.id}</td>
-        <td rowSpan={lights.length}>{signal.operationMode}</td>
-        <td rowSpan={lights.length}>{signal.location}</td>
+        <th rowSpan={lights.length} width='10px'>{signal.id}</th>
+        <td rowSpan={lights.length} width='50px'>{signal.location}</td>
         <td rowSpan={lights.length}>{signal.lat}</td>
         <td rowSpan={lights.length}>{signal.lng}</td>
-        <td rowSpan={lights.length}>{`local:${time.toLocaleTimeString()} \n epoch:${time.valueOf()}`}</td>
-        <td rowSpan={lights.length}>{lights.length}</td>
+        <td rowSpan={lights.length}>
+            <table className='table table-bordered mb-0'>
+                <tbody>
+                <tr>
+                    <td className='table-active'>Sync Time</td>
+                    <td>{new Date(signal.timer).toLocaleTimeString('en-IN', {
+                        year: 'numeric',
+                        month: 'numeric',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        second: 'numeric'
+                    })}</td>
+                </tr>
+                <tr>
+                    <td className='table-active'>Since Reset</td>
+                    <td className='timer'>
+                        <span className="timeBar" style={timerBarStyle}></span>
+                        <span className="time">{ratioTimeModulo}</span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+        <td rowSpan={signal.lights.length}>
+            <table className='table table-bordered mb-0'>
+                <thead className='table-active'>
+                <tr>
+                    <td>Red</td>
+                    <td>Green</td>
+                </tr>
+                </thead>
+                <tbody>
+                {signal.controlList.map((control, i) => <tr key={i}>
+                    <td>{control.red.toString()}</td>
+                    <td>{control.green.toString()}</td>
+                </tr>)}
+                </tbody>
+            </table>
+        </td>
+        <td rowSpan={lights.length} width='10px'>{lights.length}</td>
     </Fragment>
 
-    lights = lights.map((light, i) => <tr key={light.id}>
+    return lights.map((light, i) => <tr key={light.id}>
         {i === 0 ? main : null}
-        <td>{light.id}</td>
+        <th width='10px'>{light.id}</th>
         <td>{light.direction}</td>
         <td>{light.operationMode}</td>
-        <td>{light.signalState}</td>
+        <td className={light.signalState === 'Red' ? 'bgred' : 'bggreen'}>
+            {light.signalState}
+        </td>
     </tr>)
 
-    return lights;
 }
 
 export default Signal;
--- src/components/SignalList.js
+++ src/components/SignalList.js
@@ -3,39 +3,54 @@ import axios from '../axios';
 import Signal from "./Signal";
 
 const SignalList = () => {
-    const [signals, setSignals] = useState([])
+    const [signals, setSignals] = useState([]);
+    const [resetInterval, setResetInterval] = useState(5);
+    const [currTime, setCurTime] = useState(new Date());
 
-    useEffect(() => {
+    const fetchReport = () => {
         axios.get('/maps/stateReporting/')
             .then(response => {
-                setSignals(response.data);
-                console.log(response.data)
+                setSignals(response.data.signals);
+                setResetInterval(response.data.resetInterval);
             }).catch(err => {
             console.log(err);
         })
-    }, []);
+    }
 
-    const signalsDiv = signals.map((signal, i) => <Signal key={i} signal={signal}/>)
+    useEffect(() => {
+        // fetchReport();
+        const intervalObj = setInterval(() => {
+            fetchReport();
+            setCurTime(new Date());
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalObj)
+        }
+    });
+
+    const signalsDiv = signals.map((signal, i) => <Signal key={i} currTime={currTime} resetInterval={resetInterval}
+                                                          signal={signal}/>)
 
     return <Fragment>
         <table className='table table-bordered'>
             <thead className='table-dark'>
-                <tr>
-                    <td>Signal Id</td>
-                    <td>Mode</td>
-                    <td>Location</td>
-                    <td>Lat</td>
-                    <td>Lng</td>
-                    <td>Timer</td>
-                    <td>No.Lights</td>
-                    <td>Light Id</td>
-                    <td>Direction</td>
-                    <td>Mode</td>
-                    <td>Color</td>
-                </tr>
+            <tr>
+                <td>Signal Id</td>
+                <td>Location</td>
+                <td>Lat</td>
+                <td>Lng</td>
+                <td>Timer</td>
+                <td>Control List</td>
+                <td>No. Lights</td>
+                <td>Light Id</td>
+                <td>Direction</td>
+                <td>Mode</td>
+                <td>Color</td>
+            </tr>
             </thead>
             <tbody>
-                {signalsDiv}
+            {signalsDiv}
             </tbody>
         </table>
     </Fragment>
