47196251388c7f24cf25d9382ab1e6ff75e81df8
Naveen Sundar <naveennvrgup@gmail.com>
2020-10-22 15:41:03 +0530

    build: heartbeat

---
+++ build/precache-manifest.1fdc77b5a75638f2f12e620d486956e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c98020d79377055c28a07fd21e9367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a30973f90b01454676ba",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "78ccc2c5806d38bfa080",
+    "url": "/static/css/main.4084b6d0.chunk.css"
+  },
+  {
+    "revision": "a30973f90b01454676ba",
+    "url": "/static/js/2.5e0605ba.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5e0605ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ccc2c5806d38bfa080",
+    "url": "/static/js/main.c3d3b1b0.chunk.js"
+  },
+  {
+    "revision": "c9da2e7832b85e452e48",
+    "url": "/static/js/runtime-main.0006d2b2.js"
+  }
+]);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+let baseURL = 'http://0.0.0.0:8000';
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL
 });
 
 instance.defaults.headers.common['Authorization'] = 'Token dbef64a307efc2df5a8cab4827a8a65833f1b5e6';
